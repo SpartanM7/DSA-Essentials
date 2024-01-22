62c9449498946b0ab9c4558cb20edb4132a63a05
Maaz Ahmed <maazahmed2605@gmail.com>
2024-01-23 01:45:55 +0530

    added LL approch need to add more approach

--- LeetCode/2.add-two-numbers.cpp
+++ LeetCode/2.add-two-numbers.cpp
@@ -18,7 +18,28 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        
+        int carry = 0;
+        int sum = 0;
+        ListNode* dummyHead = new ListNode(0);
+        ListNode* current = dummyHead;
+        // missing the carry condition
+        while(l1 != NULL || l2 != NULL || carry){
+            sum = 0;
+            if(l1 != NULL){
+                sum = sum + l1->val;
+                l1 = l1->next;
+            }
+            if(l2 != NULL){
+                sum += l2->val;
+                l2 = l2->next;
+            }
+            sum += carry; 
+            carry = sum / 10;
+            current->next = new ListNode(sum % 10);
+            current = current->next;
+        }
+    
+        return dummyHead->next;
     }
 };
 // @lc code=end
